4957dfd0cc867116f40707c69c09a7b634f63587
Bruce Delo <bruce.delo@gmail.com>
2024-02-28 21:42:44 -0400

    Cleaned up and solidified at least the main chunk of the code- which takes the detection extract and the path to the OBIS occurrence file and returns a cropped (or not) shapefile that can be then (hopefully, we'll test this when the tests are working) plugged into remora QC.

--- R/get_qc_shapes.R
+++ R/get_qc_shapes.R
@@ -2,8 +2,8 @@
 ##' 
 ##' @description generate shapefiles and transition layers for QC based on user-supplied parameters
 ##' 
-##' @param detection_extract ...
-##' @param shapefile ...
+##' @param detection_extract Either a path to a detection extract file, or a dataframe representing the detection extract.
+##' @param shapefile_path Path to an OBIS occurrence file from which a shapefile can be derived with getDynamicAlphaHull
 ##' @param worldimage ...
 ##' 
 ##' @return a list of cropped shapefile, transition layer, and world raster
@@ -15,42 +15,56 @@
 ##' @keywords internal
 
 get_qc_shapes <- function(detection_extract,
-                          shapefile,
-                          worldimage = "./testDataOTN/NE2_50M_SR.tif") {
+                          shapefile_path,
+                          crop = FALSE,
+                          worldimage = "./testDataOTN/NE2_50M_SR.tif")  {
   
-
-  if(!inherits(detection_extract, "sf")) {
-    minLat = min(detection_extract$latitude) - 5
-    minLon = min(detection_extract$longitude) - 5
-    maxLat = max(detection_extract$latitude) + 5
-    maxLon = max(detection_extract$longitude) + 5
-    #Crop the range shapefile
-    shapefile_crop <- st_crop(shapefile,  xmin=minLon, ymin=minLat, xmax=maxLon, ymax=maxLat)
-  } else {
-    ext <- st_bbox(detection_extract)
-    ext[1] <- ext[1] + 5 * sign(ext[1])
-    ext[2] <- ext[2] + 5 * sign(ext[2])
-    ext[3] <- ext[3] + 5 * sign(ext[3])
-    ext[4] <- ext[4] + 5 * sign(ext[4])
-    #Crop the range shapefile
-    shapefile_crop <- st_crop(shapefile,  ext)
+  #If the detection extract has been passed as a filepath rather than a dataframe, then just read it in. 
+  if (is.character(detection_extract)) {
+    detection_extract <- read_csv(detection_extract)
   }
-
-  #Generate a transition layer
-  transition_layer <- make_transition2(shapefile_crop)
   
+  #Get the shapefile from the shapefile path:
+  species_range_raw <- read_csv(shapefile_path)
+  species_range <- species_range_raw %>% filter(is.na(flags))
+  #Gotta bust these args up to the user level.
+  species_polygon_hull <- getDynamicAlphaHull(species_range, fraction = 0.70, buff = 1000, partCount = 20, coordHeaders = c("decimalLongitude", "decimalLatitude"), clipToCoast = "aquatic")
+  species_polygon <- species_polygon_hull[[1]]
+  
+  if (crop) {
+    if(!inherits(detection_extract, "sf")) {
+      minLat = min(detection_extract$latitude) - 5
+      minLon = min(detection_extract$longitude) - 5
+      maxLat = max(detection_extract$latitude) + 5
+      maxLon = max(detection_extract$longitude) + 5
+      #Crop the range shapefile
+      species_polygon <- st_crop(species_polygon,  xmin=minLon, ymin=minLat, xmax=maxLon, ymax=maxLat)
+    } else {
+      ext <- st_bbox(detection_extract)
+      ext[1] <- ext[1] + 5 * sign(ext[1])
+      ext[2] <- ext[2] + 5 * sign(ext[2])
+      ext[3] <- ext[3] + 5 * sign(ext[3])
+      ext[4] <- ext[4] + 5 * sign(ext[4])
+      #Crop the range shapefile
+      species_polygon <- st_crop(species_polygon, ext)
+    }
+  }
+  
+  return (species_polygon)
+  
+  #These are leftover from the OG version of this code; they gotta get cleaned up and fixed but the above works pretty OK so we'll
+  #stick with it and use it for what it is.
   #Crop the world raster
-  if(file.exists(worldimage)) {
-    world_raster <- raster(worldimage)
-  }
-  else {
-    world_raster <- worldimage
-  }
+  #if(file.exists(worldimage)) {
+  #  world_raster <- raster(worldimage)
+  #}
+  #else {
+  #  world_raster <- worldimage
+  #}
   
-  world_raster_crop <- crop(world_raster, shapefile_crop)
+  #world_raster_crop <- crop(world_raster, shapefile_crop)
   
-  return(
-    list(shapefile_crop, transition_layer, world_raster_crop)
-  )
-  
+  #return(
+  #  list(shapefile_crop, transition_layer, world_raster_crop)
+  #)
 }
