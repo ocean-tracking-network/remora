355858b21d8c10175eddac0087ccdf64d2055827
Bruce Delo <bruce.delo@gmail.com>
2024-04-26 13:19:12 -0300

    Deleted a few needless files, also tweaked qc to use shortest_dist 2 instead of shortest_dist.

--- R/raster_from_griddap.R
+++
@@ -1,6 +0,0 @@
-raster_from_griddap <- function(griddap, var) {
-  #Get the filepath to the saved netcdf file associated with the griddap object.
-  filename <- griddap$summary['filename']
-  
-  
-}
