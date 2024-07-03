2943868c15c6ba89c9beb51dd26ca10d10a6feb2
Bruce Delo <bruce.delo@gmail.com>
2024-07-03 13:56:23 -0300

    Committing before branch swap.

--- ACT_Remora_workup.R
+++ ACT_Remora_workup.R
@@ -15,38 +15,50 @@ library(geosphere)
 library(rangeBuilder)
 library(surimi)
 
-devtools::install_github("ocean-tracking-network/remora@nicerHulls", force=TRUE)
+devtools::install_github("ocean-tracking-network/remora", force=TRUE)
 library(remora)
+
+#setwd('Work/remora')
+
+#download.file("https://members.oceantrack.org/data/share/testdataotn.zip/@@download/file/testDataOTN.zip", "./testDataOTN.zip")
+#unzip("testDataOTN.zip")
 
 world_raster <- raster::raster("./testDataOTN/NE2_50M_SR.tif")
 
-tests_vector <-  c("FDA_QC",
-                   "Velocity_QC",
-                   "Distance_QC",
-                   "DetectionDistribution_QC",
-                   "DistanceRelease_QC",
-                   "ReleaseDate_QC",
-                   "ReleaseLocation_QC",
-                   "Detection_QC")
+tests_vector <-  c("FDA_QC", #Is the detection likely to be false based on the Pincock/Remora algorithm?
+                   "Velocity_QC", #Did the fish travel unrealistically fast?
+                   "Distance_QC", #Did the fish travel unrealistically far?
+                   "DetectionDistribution_QC", #Was the fish detected outside its known home range?
+                   "DistanceRelease_QC", #Was the fish detected unrealistically far from the release?
+                   "ReleaseDate_QC", #Was the fish detected before the release date?
+                   "ReleaseLocation_QC", #Was the release location within the home range OR within 500km of the first detection? 
+                   "Detection_QC") #Aggregation of all the above tests, returning a value between 1 and 4.
 
-otn_files_ugacci <- list(det = "/Users/bruce/Downloads/ugaaci_matched_detections_2017/ugaaci_matched_detections_2017.csv")
+otn_files_ugacci <- list(det = "./testDataOTN/ugaaci_matched_detections_2017.csv")
 
 scientific_name <- "Acipenser oxyrinchus"
 
+#Mostly work by Steve Formel!
+#Takes just under one minute to run
 sturgeonOccurrence <- getOccurrence(scientific_name)
 
+#Mostly work by Jessica Castellanos!
+#Takes just under 90 seconds to run.
 sturgeonPolygon <- createPolygon(sturgeonOccurrence, fraction=1, partsCount=1, buff=100000, clipToCoast = "aquatic")
 
+plot(sturgeonPolygon)
+
+#Takes about 5.5m to run.
 otn_test_tag_qc <- runQC(otn_files_ugacci, 
                          data_format = "otn", 
                          tests_vector = tests_vector, 
                          shapefile = sturgeonPolygon, 
-                         col_spec = NULL, 
                          fda_type = "pincock", 
                          rollup = TRUE,
                          world_raster = world_raster,
                          .parallel = FALSE, .progress = TRUE)
 
+#Mostly work by Ian Jonsen!
 plotQC(otn_test_tag_qc, distribution_shp = sturgeonPolygon, data_format = "otn")
 
 
@@ -54,10 +66,17 @@ otn_files_fsugg <- list(det = "/Users/bruce/Downloads/fsugg_matched_detections_2
 
 scientific_name <- "Epinephelus itajara"
 
+#Takes about 3.75m to run.
 grouperOccurrence <- getOccurrence(scientific_name)
 
-grouperPolygon <- createPolygon(grouperOccurrence, fraction=1, partsCount=1, buff=10000, clipToCoast = "aquatic")
+#OBIS is a data system populated by people observing biodiversity. So, it's very good about having data where there are people. - Some paper Jon read.
+#grouperPolygon <- createPolygon(grouperOccurrence, fraction=1, partsCount=1, buff=10000, clipToCoast = "aquatic")
+#Takes about 1.6m to run.
+grouperPolygon <- createPolygon(grouperOccurrence, buff=2000, clipToCoast = "aquatic")
 
+plot(grouperPolygon)
+
+#Takes about 6m to run.
 otn_test_tag_qc <- runQC(otn_files_fsugg, 
                          data_format = "otn", 
                          tests_vector = tests_vector, 
